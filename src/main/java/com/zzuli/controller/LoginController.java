e1996d37a7ba30504cf57d0edce21b345b58607b
itgy <3563433349@qq.com>
2023-03-22 16:01:51 +0800

    小修改

--- src/main/java/com/zzuli/controller/LoginController.java
+++ src/main/java/com/zzuli/controller/LoginController.java
@@ -21,16 +21,4 @@ public class LoginController {
     {
         return "登陆成功";
     }
-    @GetMapping("/text")
-    GlobalExceptionHandler @PreAuthorize("hasRole('ROLE_SEARCH')")
-    public String text()
-    {
-        return "测试成功";
-    }
-    @GetMapping("/text2")
-    @PreAuthorize("hasRole('pp')")
-    public String text2()
-    {
-        return "测试成功2";
-    }
 }
